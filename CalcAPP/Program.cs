dab421d75f3b9788a05347f4545a8e0c6a843d22
unknown <christega294@gmail.com>
2022-08-21 00:34:37 -0700

    initial commit

---
+++ CalcAPP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalcAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A simple calculator
+            Console.WriteLine("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter your operator");
+            string ops = Console.ReadLine();
+
+            Console.WriteLine("Enter another number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (ops == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (ops == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (ops == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (ops == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (ops == "%")
+            {
+                Console.WriteLine(num1 % num2);
+            }
+            else
+            {
+                Console.WriteLine("invalid operator");
+             }
+
+
+        }
+    }   
+}
